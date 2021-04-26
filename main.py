ff709978dbb1826f0791e0a8b9cb6a905fa47467
Christian Kitte <chkitte@web.de>
2021-04-26 21:39:24 +0200

    Weiter an erster Implementierung gearbeitet. URLIB hinzugefügt

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
 from __future__ import division
 
-import bs4.element
-import requests
-import re
+from urllib import request
+
 from bs4 import BeautifulSoup
 
-# Press the green button in the gutter to run the script.
+
+def get_soup_by_url(target_url, page_encoding="utf-8"):
+    target_request = request.urlopen(target_url)
+    raw_page = target_request.read().decode(page_encoding)
+    return BeautifulSoup(raw_page, features="html.parser")
+
+
+def get_soup_by_page(raw_html_page, page_encoding="utf-8"):
+    raw_page = open(raw_html_page, encoding=page_encoding, mode="r").read().strip()
+    return BeautifulSoup(raw_page, features="html.parser")
+
+def get_div_by_class(div_class):
+    return soup.find_all("div", attrs={"class": div_class})
+
+
+
+# https://www.codegrepper.com/code-examples/whatever/get+button+in+div+without+if+or+class+beautifulsoup
+# https://www.python-lernen.de/bibliothek-urllib.htm
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/index.html?highlight=findall#tag
+# https://dev.to/joks84/playing-with-beautifulsoup-spiders-might-not-be-so-scary-after-all-4o81
+
+
 if __name__ == '__main__':
-    page = requests.get("https://www.web.de")
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.content, "html.parser")
+    # print(get_soup_by_url("https://news.google.com"))
+    # print(get_soup_by_page("trump.txt"))
 
-        for link in soup.findAll('a', attrs={'href': re.compile("^https://")}):
-            x= link.get("href")
-            print(x)
+    # soup = get_soup_by_page("trump.txt")
+    soup = get_soup_by_page("trump_verschwörung.html")
+    #print(soup.prettify())
 
+
+    #test= soup.find_all("div",attrs={"class":"g"})
+    #test= soup.find_all("span",attrs={"class":"aCOpRe"})
+    for x in get_div_by_class("g"):
+        print("Start")
+        print(x)
+        print("End")
+
+
+
+    #print(test)
+    #g=0
+    #class ="IsZvec"
+    #print(soup.find_all("a"))
+    # = soup.find_all("div")
+    #print(r[0])
